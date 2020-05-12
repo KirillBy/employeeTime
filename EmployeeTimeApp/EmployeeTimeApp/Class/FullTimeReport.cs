8f434b6de9f05dfc4a0e904a786d1986f23b6609
Kiryl Balanovich <balanovichks@gmail.com>
2020-05-12 16:14:02 +0300

    Entity connected

---
+++ EmployeeTimeApp/EmployeeTimeApp/Class/FullTimeReport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTimeApp
+{
+    internal class FullTimeReport
+    {
+        public string EmployeeName { get; set; }
+        public float WorkHours { get; set; }
+        public DateTime WorkDay { get; set; }
+    }
+}
--- EmployeeTimeApp/EmployeeTimeApp/Program.cs
+++ EmployeeTimeApp/EmployeeTimeApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,30 @@ namespace EmployeeTimeApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var timeReports = GetTimeReports();
+
+            foreach (var timeReport in timeReports)
+            {
+                Console.WriteLine("{0} works {1} hours at {2}  {3}", timeReport.EmployeeName, timeReport.WorkHours,
+                    timeReport.WorkDay.ToString("M/d/yyyy"), timeReport.WorkDay.DayOfWeek);
+            }
+            Console.ReadLine();
+        }
+
+        private static IQueryable<FullTimeReport> GetTimeReports()
+        {
+            var context = new EmployeeDbContext();
+            var timeReports = from tr in context.TimeReports
+                              join e in context.Employee on tr.employee_id equals e.id
+                              select new FullTimeReport()
+                              {
+                                  EmployeeName = e.name,
+                                  WorkHours = (float)tr.hours,
+                                  WorkDay = tr.date
+                              };
+            return timeReports;
         }
     }
 }
